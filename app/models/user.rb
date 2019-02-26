11b1d0c573f2876dd45912ca307ffee07fd9d35e
tomoyukiito <ito@nagisa-inc.jp>
2019-02-26 17:26:57 +0900

    feat: add carrierwave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213061855) do
+ActiveRecord::Schema.define(version: 20190226073707) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
