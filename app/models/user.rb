95fa9bd409731ee559b19c5f08c8553d5018a5bb
tomoyukiito <ito@nagisa-inc.jp>
2019-02-27 12:42:07 +0900

    feat(user): add gender as enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :age, presence: true
   mount_uploader :image, ImageUploader
+  enum gender: { male: 1, female: 2 }
 end
